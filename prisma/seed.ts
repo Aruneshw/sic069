f768981c8d6a1957616cc5fdbd63807471c49611
Aruneshw <aruneshownsty1@gmail.com>
2026-08-10 15:11:51 +0530

    Remove datasourceUrl from PrismaClient constructors

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "../src/generated/prisma/client";
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL
-});
+const prisma = new PrismaClient();
 
 
 
